2b4632aec6d15217dcd943b57e5a01b3d5380586
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2024-01-13 12:08:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -33,6 +33,26 @@ const generate = async(browser,page,title,year)=>{
 	}
 	return dt
 }
+const generateTv = async(browser,page,title,epsoide,season)=>{
+	await page.goto(`https://weciimaa.online/watch/%d9%85%d8%b4%d8%a7%d9%87%d8%af%d8%a9-%d9%85%d8%b3%d9%84%d8%b3%d9%84-${title}-%d9%85%d9%88%d8%b3%d9%85-${season}-%d8%ad%d9%84%d9%82%d8%a9-${epsoide}/`);
+	const searchResultSelector = '.List--Download--Wecima--Single a';
+	
+	const textSelector = await page.$$(searchResultSelector);
+	// console.log(textSelector.length);
+	const dt = []
+	for(let i=0;i<textSelector.length;i++){
+		
+		const d =await textSelector[i]?.evaluate(el =>{
+			return ({
+				url:el.href,
+				res:el.children[1]?.textContent,
+				q:el.children[0]?.textContent
+			})
+		});
+		dt.push(d)
+	}
+	return dt
+}
 const app = (browser,page)=>{
     appExpress.use(express.json());
 appExpress.use(express.urlencoded({ extended: false }))
@@ -52,6 +72,21 @@ appExpress.use(cors());
             res.status(500).json(error)
         }
    })
+   appExpress.post('/get-links-tv',async(req,res)=>{
+	const {name,epsoide,season} = req.body
+	const correctName = name.replace(/[^a-zA-Z0-9: ]/g, "").split(' ').join('-')
+	
+	try {
+		const data = await generateTv(browser,page,correctName,epsoide,season)
+		if(data){
+			res.status(200).json(data)
+		}else{
+			res.status(404).json({msg:'not found'})
+		}
+	} catch (error) {
+		res.status(500).json(error)
+	}
+})
    appExpress.listen(8800, () => {
     console.log("Backend server is running!");
    });
