e4445ac3db907e481b3ef0820ed959a26750d571
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2024-08-14 21:23:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -45,15 +45,23 @@ const generate3 = async(browser,page,title,year)=>{
 }
 const generate = async(browser,page,title,year)=>{
     // Navigate the page to a URL
-	await page.goto(`https://xn------nzebca5gd7ode0biglh.myciima-weciima.shop/watch/%d9%85%d8%b4%d8%a7%d9%87%d8%af%d8%a9-%d9%81%d9%8a%d9%84%d9%85-${title.toLowerCase()}-${year}-%d9%85%d8%aa%d8%b1%d8%ac%d9%85/`);
-   
-	// // Set screen size
-	// await page.setViewport({width: 1080, height: 1024});
-	// await page.screenshot({path:'example.png'})
-	// // Type into search box
-	// await page.type('.search-box__input', 'automate beyond recorder');
-    //  console.log('done')
-	// // Wait and click on first result
+	await page.setRequestInterception(true);
+    page.on("request", (req) => {
+		const requestUrl = req.url();
+		if (!req.isInterceptResolutionHandled())
+		  if (
+			blockResourceType.includes(req.resourceType()) ||
+			blockResourceName.some((resource) => requestUrl.includes(resource))
+		  ) {
+			req.abort();
+		  } else {
+			req.continue();
+		  }
+	  });
+//https://xn------nzebca5gd7ode0biglh.myciima-weciima.shop/watch/
+	await page.goto(`https://wecima.show/watch/%d9%85%d8%b4%d8%a7%d9%87%d8%af%d8%a9-%d9%81%d9%8a%d9%84%d9%85-${title.toLowerCase()}-${year}-%d9%85%d8%aa%d8%b1%d8%ac%d9%85/`);
+
+
 	const searchResultSelector = '.List--Download--Wecima--Single a';
 	
 	const textSelector = await page.$$(searchResultSelector);
@@ -70,6 +78,7 @@ const generate = async(browser,page,title,year)=>{
 		});
 		dt.push(d)
 	}
+	
 	return dt
 }
 const generateTv = async(browser,page,title,epsoide,season)=>{
@@ -112,16 +121,17 @@ appExpress.use(cors());
         res.status(500).json(error)
     }
 })
-    appExpress.post('/get-links',async(req,res)=>{
-        const {name,year} = req.body
+    appExpress.get('/get-links',async(req,res)=>{
+		const {name,year} = req.query
+		console.log(req.query)
         const correctName = name.replace(/[^a-zA-Z0-9: ]/g, "").split(' ').join('-')
-        
         try {
             const data = await generate(browser,page,correctName,year)
+			console.log(data)
             if(data){
                 res.status(200).json(data)
             }else{
-                res.status(404).json({msg:'not found'})
+                res.status(403).json({msg:'not found'})
             }
         } catch (error) {
             res.status(500).json(error)
