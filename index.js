2d8016b549f9de2278fa3fb2a88624809796375b
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2024-08-18 21:52:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const init = async()=>{
 		const browser = await puppeteer.launch(
 			{
     args:[
-        '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'
+        "--disable-setuid-sandbox",
+				  "--no-sandbox",
+				  "--single-process",
+				  "--no-zygote",
       ],
       executablePath:process.env.NODE_ENV === "production"?process.env.PUPPETEER_EXECUTABLE_PATH:puppeteer.executablePath(),
       headless:"new",
