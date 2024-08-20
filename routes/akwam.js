83c3ee790b79cb78ff8e986553319db360a71acf
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2024-08-20 17:34:19 +0300

    add new route for tv shows

--- routes/akwam.js
+++ routes/akwam.js
@@ -1,7 +1,8 @@
-const {  generateServer1 } = require('../controllers/akwam')
+const {  generateServer1,  getEpsoide  } = require('../controllers/akwam')
 
 const router = require('express').Router()
 
 router.get('/',generateServer1)
+router.get('/tv/',getEpsoide)
 router.get('/list',)
 module.exports = router
